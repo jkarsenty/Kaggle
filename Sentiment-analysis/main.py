c05957cdc8391be7463c90739275e7e5bab2aaf1
jkarsenty <karsentyjk@gmail.com>
2020-04-22 15:10:51 +0200

    init

--- Sentiment-analysis/main.py
+++ Sentiment-analysis/main.py
@@ -4,3 +4,9 @@
 from import_data import importation
 
 data = importation("data/train.csv")
+print(data.columns)
+#print(data['text'].head())
+
+## on realise une premiere classification juste sur juste le sentiment analysis
+df1 = data.drop('selected_text', axis = 1)
+print(df1.head())
